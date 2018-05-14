3fac8c95eabd46a5e9341c7afdd767cd45337dfb
Hale Deng <haledeng88@gmail.com>
2018-05-14 22:44:50 +0800

    add comments

--- build/utils.js
+++ build/utils.js
@@ -1,10 +1,20 @@
+/**
+ * 处理css和css的预处理器以及图片、多媒体文件等样式文件的方法
+ */
 'use strict'
 const path = require('path')
 const config = require('../config')
+// 提取css样式的库
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const packageConfig = require('../package.json')
 
+/**
+ * css路径
+ * @param _path
+ * @return {*|string}
+ */
 exports.assetsPath = function (_path) {
+  // css文件放置的路径
   const assetsSubDirectory = process.env.NODE_ENV === 'production'
     ? config.build.assetsSubDirectory
     : config.dev.assetsSubDirectory
@@ -12,6 +22,11 @@ exports.assetsPath = function (_path) {
   return path.posix.join(assetsSubDirectory, _path)
 }
 
+/**
+ * css解析器
+ * @param options
+ * @return {{css: *, postcss: *, less: *, sass: *, scss: *, stylus: *, styl: *}}
+ */
 exports.cssLoaders = function (options) {
   options = options || {}
 
@@ -30,7 +45,14 @@ exports.cssLoaders = function (options) {
   }
 
   // generate loader string to be used with extract text plugin
+  /**
+   * 配置css预处理解析器
+   * @param loader
+   * @param loaderOptions
+   * @return {*}
+   */
   function generateLoaders (loader, loaderOptions) {
+    // 通过传入的参数的usePostCSS确定是否需要postcssLoader
     const loaders = options.usePostCSS ? [cssLoader, postcssLoader] : [cssLoader]
 
     if (loader) {
@@ -44,6 +66,7 @@ exports.cssLoaders = function (options) {
 
     // Extract CSS when that option is specified
     // (which is the case during production build)
+    // 通过传入的参数的extract确定是否需要提取css样式，ExtractTextPlugin需要配置use和fallback
     if (options.extract) {
       return ExtractTextPlugin.extract({
         use: loaders,
@@ -67,6 +90,11 @@ exports.cssLoaders = function (options) {
 }
 
 // Generate loaders for standalone style files (outside of .vue)
+/**
+ * 生成输出
+ * @param options
+ * @return {Array}
+ */
 exports.styleLoaders = function (options) {
   const output = []
   const loaders = exports.cssLoaders(options)
@@ -82,6 +110,10 @@ exports.styleLoaders = function (options) {
   return output
 }
 
+/**
+ * 跨平台消息通知
+ * @return {Function}
+ */
 exports.createNotifierCallback = () => {
   const notifier = require('node-notifier')
 
