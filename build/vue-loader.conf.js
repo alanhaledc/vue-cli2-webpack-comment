cc109e7f24e955638dab21a5daab0e02dc51e8e6
Hale Deng <haledeng88@gmail.com>
2018-05-14 22:45:08 +0800

    add comments

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -1,3 +1,6 @@
+/**
+ * vue-loader解析器的配置
+ */
 'use strict'
 const utils = require('./utils')
 const config = require('../config')
@@ -8,11 +11,14 @@ const sourceMapEnabled = isProduction
 
 module.exports = {
   loaders: utils.cssLoaders({
+    // 根据相关配置确定是否开启
     sourceMap: sourceMapEnabled,
+    // 生产环境提取
     extract: isProduction
   }),
   cssSourceMap: sourceMapEnabled,
   cacheBusting: config.dev.cacheBusting,
+  // 转换成require调用
   transformToRequire: {
     video: ['src', 'poster'],
     source: 'src',
