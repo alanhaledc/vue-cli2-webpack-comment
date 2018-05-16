17caf0268ece0c9af3b6119e12eb848248f64497
Hale Deng <haledeng88@gmail.com>
2018-05-16 14:54:00 +0800

    update comments

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+// 路由懒加载，测试webpackConfig.output.chunkFilename
+const HelloWorld = () => import('@/components/HelloWorld')
 
 Vue.use(Router)
 
